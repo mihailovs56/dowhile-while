46bafb99205f784f81b4e11cbde7a90d18175d20
Михайлов <@DESKTOP-84VD5MI>
2023-10-20 20:56:07 +0500

    Добавьте файлы проекта.

---
+++ Практика5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практика5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Выводится цикл dowhile: ");
+            do
+            {
+                Console.WriteLine(a);
+                a--;
+            }
+            while (a > 0);
+            Console.WriteLine("Введите такое же число, как и первое: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Выводится цикл while: ");
+            while (b > 0)
+            {
+                Console.WriteLine(b);
+                b--;
+            }
+        }
+    }
+}
